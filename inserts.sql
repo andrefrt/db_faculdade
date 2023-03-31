c127c9aa1911c144a32f6fa6d0cbdd807389f51b
andrefrt <116937559+andrefrt@users.noreply.github.com>
2023-03-31 13:53:51 -0300

    Update inserts.sql

--- inserts.sql
+++ inserts.sql
@@ -30,3 +30,26 @@ INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Júlia Cunha Li
 INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Letícia Rodrigues Cavalcanti',75286247097,'F','18');
 INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Larissa Rodrigues Lima',64917140778,'F','17');
 INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Raissa Carvalho Alves',77908350167,'F','24');
+
+-- INSERTS PROFESSORES
+
+INSERT INTO professor (professor_nome, professor_cpf, genero, idade, salario) VALUES ('Tabbie Wythe', '05659901249', 'M', '32', 5616.17);
+INSERT INTO professor (professor_nome, professor_cpf, genero, idade, salario) VALUES ('Kizzie BURWIN', '02053536086', 'M', '84', 5909.35);
+INSERT INTO professor (professor_nome, professor_cpf, genero, idade, salario) VALUES ('Peggi Lorans', '66069551325', 'F', '99', 2419.35);
+INSERT INTO professor (professor_nome, professor_cpf, genero, idade, salario) VALUES ('Marena Arnli', '19294812280', 'F', '68', 5050.68);
+INSERT INTO professor (professor_nome, professor_cpf, genero, idade, salario) VALUES ('Roda Strickler', '50442108270', 'M', '47', 3095.07);
+INSERT INTO professor (professor_nome, professor_cpf, genero, idade, salario) VALUES ('Mariele Powder', '77840210561', 'M', '04', 4835.06);
+INSERT INTO professor (professor_nome, professor_cpf, genero, idade, salario) VALUES ('Marianne Moorcraft', '28614072222', 'F', '20', 1065.54);
+INSERT INTO professor (professor_nome, professor_cpf, genero, idade, salario) VALUES ('Rona Sambell', '88913998084', 'M', '86', 3613.10);
+INSERT INTO professor (professor_nome, professor_cpf, genero, idade, salario) VALUES ('Robbert Nosworthy', '53884274893', 'F', '29', 4548.98);
+INSERT INTO professor (professor_nome, professor_cpf, genero, idade, salario) VALUES ('Dyann Trewin', '85946995529', 'F', '08', 4384.60);
+INSERT INTO professor (professor_nome, professor_cpf, genero, idade, salario) VALUES ('Delly Zamorrano', '04951505517', 'M', '15', 1704.74);
+INSERT INTO professor (professor_nome, professor_cpf, genero, idade, salario) VALUES ('Devinne Burbridge', '52699889820', 'F', '90', 1653.55);
+INSERT INTO professor (professor_nome, professor_cpf, genero, idade, salario) VALUES ('Dede Gasparro', '80573927735', 'M', '43', 5010.15);
+INSERT INTO professor (professor_nome, professor_cpf, genero, idade, salario) VALUES ('Melamie Franzolini', '39981623587', 'M', '69', 6491.10);
+INSERT INTO professor (professor_nome, professor_cpf, genero, idade, salario) VALUES ('Ruprecht Kilday', '66931685553', 'M', '93', 2289.69);
+INSERT INTO professor (professor_nome, professor_cpf, genero, idade, salario) VALUES ('Oates Sorbie', '46222980946', 'F', '56', 5435.63);
+INSERT INTO professor (professor_nome, professor_cpf, genero, idade, salario) VALUES ('Gunner Studdert', '22617885740', 'M', '44', 2234.42);
+INSERT INTO professor (professor_nome, professor_cpf, genero, idade, salario) VALUES ('Julienne Proppers', '28965702534', 'F', '96', 3127.58);
+INSERT INTO professor (professor_nome, professor_cpf, genero, idade, salario) VALUES ('Tobiah Gyngyll', '72096757507', 'F', '65', 6810.51);
+INSERT INTO professor (professor_nome, professor_cpf, genero, idade, salario) VALUES ('Brand Audiss', '40250529045', 'M', '50', 3897.76);
