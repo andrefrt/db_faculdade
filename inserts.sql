febc4ccb0a9e4ddca569a5baf50469368e3d6290
andrefrt <116937559+andrefrt@users.noreply.github.com>
2023-03-31 13:23:52 -0300

    Create inserts.sql

---
+++ inserts.sql
@@ -0,0 +1,32 @@
+--INSERTS ALUNO
+
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Rafael Almeida Azevedo',65155524407,'M','18');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Vinícius Correia Barbosa',49266274348,'M','19');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Kauan Goncalves Costa',97853049498,'M','22');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Samuel Correia Sousa',77050313459,'M','21');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Diogo Pereira Barros',12981340212,'M','24');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Marcos Cardoso Cavalcanti',59681262050,'M','18');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Felipe Santos Martins',39857808034,'M','20');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Caio Alves Cardoso',71102129470,'M','19');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Murilo Ribeiro Correia',50567895858,'M','23');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Thiago Araujo Rocha',15995592939,'M','17');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Douglas Costa Goncalves',65930479615,'M','25');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Miguel Ribeiro Fernandes',48334324553,'M','28');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Lucas Almeida Castro',19604676113,'M','17');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Carlos Costa Melo',42910023354,'M','22');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('André Rodrigues Melo',28052694469,'M','26');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Anna Ferreira Oliveira',43351938900,'F','18');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Marina Souza Lima',93110388650,'F','27');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Livia Cunha Pereira',11465693554,'F','19');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Júlia Alves Ribeiro',30088519983,'F','20');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Bruna Melo Martins',50851969370,'F','22');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Raissa Cavalcanti Correia',47278100761,'F','18');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Luana Alves Sousa',55171230008,'F','17');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Isabella Carvalho Gomes',73307679989,'F','23');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Laura Sousa Pereira',87072171313,'F','18');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Sophia Barros Alves',83617022668,'F','20');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Anna Rocha Cavalcanti',92181375653,'F','19');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Júlia Cunha Lima',38095469781,'F','21');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Letícia Rodrigues Cavalcanti',75286247097,'F','18');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Larissa Rodrigues Lima',64917140778,'F','17');
+INSERT INTO aluno(aluno_nome, aluno_cpf, genero, idade) VALUES ('Raissa Carvalho Alves',77908350167,'F','24');
